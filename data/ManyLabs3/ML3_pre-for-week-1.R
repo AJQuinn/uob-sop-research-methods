834982eae0f331c00dc68dd01facf72a1ed01304
Andrew Quinn <aquinn501@gmail.com>
2024-11-04 15:59:16 +0000

    Major updates

---
+++ data/ManyLabs3/ML3_pre-for-week-1.R
@@ -0,0 +1,15 @@
+library(dplyr)
+library(jmv)
+
+data = read.csv('data/ManyLabs3/ML3AllSites.csv')
+
+data$age <- as.numeric(as.character(data$age))
+
+data <- data %>%
+  filter(!is.na(age)) %>%
+  select(c('Participant_ID', 'session_id', 'age', 'major', 
+           'gender', 'Openness', 'Conscientiousness', 
+           'Extraversion', 'Agreeableness', 'Neuroticism',
+           'Intrinsic', 'Mood', 'NFC', 'SelfEsteem',))
+
+write.csv(data, 'data/ManyLabs3/ML3AllSites_rmb.csv')
